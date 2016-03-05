620804715706e678f97900b019afa664992e302d
Graham Cox <graham@grahamcox.co.uk>
2016-03-05 23:15:26 +0000

    IPC in React

--- src/ui/ui/MainWindow.jsx
+++ src/ui/ui/MainWindow.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import ipc from 'ipc';
 
 export const MainWindow = React.createClass({
+    getInitialState: function() {
+        return {
+            message: 'Started'
+        };
+    },
+    componentDidMount: function() {
+        ipc.on('ping', (message) => {
+            this.setState({message});
+        });
+    },
     render: function() {
         return (
             <div className="mainWindow">
-                This is the Main Window
+                <div>This is the Main Window.</div>
+                <div>The current message is: {this.state.message}</div>
             </div>
         );
     }
